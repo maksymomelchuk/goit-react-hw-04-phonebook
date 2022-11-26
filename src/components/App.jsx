973162cf8cf59f2c1f1ac08732becbb8a7de7571
maksymomelchuk <omelchukmaksym@gmail.com>
2022-11-26 08:55:21 +0200

    commit before sending to tutor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,64 @@
+import { nanoid } from 'nanoid';
+import { useState, useEffect } from 'react';
+import ContactsList from './Contacts/ContactsList';
+import Filter from './Filter/Filter';
+import { ContactForm } from './Phonebook/ContactForm';
+
+const LS_KEY = 'contact_book';
+
 export const App = () => {
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem(LS_KEY)) ?? []
+  );
+  const [filter, setFilter] = useState('');
+
+  const onFormSubmit = (name, number) => {
+    contacts.find(el => el.name === name)
+      ? alert(`${name} is already in contacts`)
+      : setContacts([
+          {
+            id: nanoid(),
+            name,
+            number,
+          },
+          ...contacts,
+        ]);
+  };
+
+  const onFilterUpdate = event => {
+    setFilter(event.currentTarget.value);
+  };
+
+  const onDeleteContact = event => {
+    const filteredContacts = contacts.filter(
+      contact => contact.id !== event.target.id
+    );
+    setContacts(filteredContacts);
+  };
+
+  useEffect(() => {
+    localStorage.setItem(LS_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+  const normalizedFilter = filter.toLowerCase();
+  const visibleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(normalizedFilter)
+  );
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1 style={{ textAlign: 'center' }}>Phonebook</h1>
+      <ContactForm onSubmit={onFormSubmit} />
+      {contacts.length !== 0 && (
+        <div>
+          <h2 style={{ textAlign: 'center' }}>Contacts</h2>
+          <Filter value={filter} onChange={onFilterUpdate} />
+          <ContactsList
+            contactsList={visibleContacts}
+            onDelete={onDeleteContact}
+          />
+        </div>
+      )}
     </div>
   );
 };
